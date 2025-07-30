b37caee97924433ccac892d70d574f188890ce16
Juan Diego <chucjuan508@gmail.com>
2025-07-30 11:49:46 -0800

    8Vo commit CALCULO TOTAL DEL INVENTARIO y cunto hay por categoria

--- Actividad 10.py
+++ Actividad 10.py
@@ -7,7 +7,7 @@ for i in range(quantity_of_profuctos_add):
         print(f"El codigo {code} de la prenda ya existe en el inventario")
         code = input("Ingrese el codigo de la prenda: ")
     name_of_product = input("Ingrese el nombre de la prenda (ej Pantalon, Playera, etc): ")
-    category_product = input("Ingrese de que categoria es la prenda (ej niño,niña,mujer): ")
+    category_product = input("Ingrese de que categoria es la prenda (ej niño,niña,mujer): ").lower()
     size_product = input("Ingrese la talla del producto (ej S, L, M, XL): ")
     unit_price = float(input("Ingrese el precio de la prenda: "))
     while unit_price <= 0:
@@ -27,11 +27,11 @@ for i in range(quantity_of_profuctos_add):
 print(f"\n ===Lista Completa de productos===")
 for code,product in inventory.items():
     print(f"\n Codigo de la prenda: {code}")
-    print(f"Nombre de la prenda: {product['name_of_product']}")
-    print(f" Categoria de la prenda: {product['category_product']}")
-    print(f"Talla de la prenda: {product['size_product']}")
-    print(f"Precio de la prenda: {product['unit_price']}")
-    print(f"Cantidad de prendas {product['stock_quantity']}")
+    print(f"Nombre de la prenda: {product.get('name_of_product')}")
+    print(f" Categoria de la prenda: {product.get('category_product', 'no esta definido')}")
+    print(f"Talla de la prenda: {product.get('size_product', 'no esta definido')}")
+    print(f"Precio de la prenda: {product.get('unit_price', 'no esta definido')}")
+    print(f"Cantidad de prendas {product.get('stock_quantity', 'no esta definido')}")
 
 if inventory:
     print("\n Busqueda de una prenda")
@@ -46,3 +46,30 @@ if inventory:
         print(f"el codigo {search_product} no existe")
 else:
     print("No se encontro ningun dato registrado en inventario ")
+print("\n Calcular el precio total del inventario")
+value_final = 0
+for price in inventory.values():
+    price_ = price['unit_price']
+    stock = price[ 'stock_quantity']
+    value_final += price_*stock
+    print(f"El precio total del inventario Q{value_final}")
+
+h =0
+m =0
+no=0
+na=0
+for datos in inventory.values():
+    categoria = datos['category_product']
+    if  categoria == "hombre":
+        h +=1
+    elif categoria == "mujer" :
+        m +=1
+    elif categoria =="niño":
+        no +=1
+    elif categoria == "niña":
+        na += 1
+print("\n Productos por cada categoria")
+print(f"Cantidad de la categoria Hombre es: {h}")
+print(f"Cantidad de la categoria Mujer es: {m}")
+print(f"Cantidad de la categoria Niña es: {na}")
+print(f"Cantidad de la categoria Niño es: {no}")
